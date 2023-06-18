66dca84f1f3eb7ec73b2d2cc048b2a2b588a210f
Dmitry <adachel74@gmail.com>
2023-06-18 18:04:23 +0500

    homeWork1

--- AlgoAndData/Seminars/seminar2.java
+++ AlgoAndData/Seminars/seminar2.java
@@ -3,10 +3,8 @@ import java.time.LocalTime;
 // import java.util.Arrays;
 // import java.util.Random;
 
-public class Main 
-{   
-    public static void main(String[] args)
-    {   
+public class seminar2 {   
+    public static void main(String[] args){   
         int[] array = new int[2000]; // Пустой массив на 20 элементов
         for(int i = 0; i < array.length; i++) array[i] = (int)(Math.random()*1000); // Заполняем массив рандомно  
         // System.out.println("arr1: " + Arrays.toString(array));   // Форма вывода массива
@@ -28,27 +26,22 @@ public class Main
         System.out.println(Duration.between(timeInsertSortStart, timeInsertSortEnd));
     }
 
-    static void quickSort(int[] array) 
-    {
+    static void quickSort(int[] array) {
         quickSortRecursive(array, 0, array.length - 1);
         // System.out.println(Arrays.toString(array));
     }
     
-    static void quickSortRecursive(int[] array, int leftBorder, int rigthBorder)
-    {
+    static void quickSortRecursive(int[] array, int leftBorder, int rigthBorder){
         int leftMarker = leftBorder;
         int rigthMarker = rigthBorder;
         int pivot = array[(leftMarker + rigthMarker) / 2];
-        do 
-        {
+        do {
             while(array[leftMarker] < pivot)
                 leftMarker ++;
             while(array[rigthMarker] > pivot)
                 rigthMarker --;
-            if(leftMarker <= rigthMarker)
-            {
-                if(leftMarker < rigthMarker)
-                {
+            if(leftMarker <= rigthMarker){
+                if(leftMarker < rigthMarker){
                     int temp = array[leftMarker];
                     array[leftMarker] = array[rigthMarker];
                     array[rigthMarker] = temp;
@@ -61,14 +54,11 @@ public class Main
         if(leftBorder < rigthMarker) {quickSortRecursive(array, leftBorder, rigthMarker);}
     }
 
-    public static void inserterSort(int[] array) 
-    {
-        for (int left = 0; left < array.length; left++) 
-        {
+    public static void inserterSort(int[] array) {
+        for (int left = 0; left < array.length; left++){
             int value = array[left];
             int i = left - 1;
-            for (; i >= 0; i--) 
-            {
+            for (; i >= 0; i--){
                 if (value < array[i]) array[i + 1] = array[i];
                 else break; 
             }
---
+++ AlgoAndData/lections/lection3.java
@@ -0,0 +1,80 @@
+public class lection3 {         // Связные списки
+    public class List{
+        Node head;              // ссылка на первую позицию
+        Node tail;              // ссылка на последнюю позицию
+        
+        public void add(int value){     // функция добавления элемента в конец списка
+            Node node = new Node();     // Создаем новую ноду
+            node.value = value;
+            if(head == null){           // определяем, есль ли уже какие либо элементы в списке, или это первая нода 
+                head = node;            // "head" становится текущей 'node'
+                tail = node;            // "tail" становится текущей 'node'
+            } 
+            else {
+                tail.next = node;   // берем текущий 'tail', его следующая 'node' равна нашей 'node'
+                node.previous = tail; // наша 'node' ссылается на текущий 'tail'
+                tail = node;        // 'tail' обновляем значением нашей 'node'
+            }
+        }
+
+        public void add(int value, Node node){ // функция добавления элемента в средину списка
+            Node next = node.next;
+            Node newNode = new Node();  // создаем новую 'node'
+            newNode.value = value;  // новая 'node' имеет следующее значение
+            node.next = newNode;    // берем текущую 'node', ее следующее значение это новая 'node'
+            newNode.previous = node; // для 'newNode', ее предыдущее значение это текущая 'node'
+            if(next == null){
+                tail = newNode;
+            }
+            else{
+                next.previous = newNode;
+                newNode.next = next;
+            }
+
+        }
+
+        public  void delete(Node node){ // удаление элемента
+            Node previous = node.previous;
+            Node next = node.next;
+            if(previous == null){   // если предыдущая 'node' равна '0'
+                next.previous = null;
+                head = next;
+            }
+            else{
+                if(next == null){
+                    previous.next = null;
+                    tail = previous;
+                }
+                else{
+                    previous.next = next;
+                    next.previous = previous;
+                }
+            }
+        }
+
+        public Node find(int value){    // метод, кот ищет нужную ноду в списке
+            Node carrentNode = head;
+            while(carrentNode != null){
+                if (carrentNode.value == value){
+                    return carrentNode;
+                }
+                carrentNode = carrentNode.next;
+            }
+            return null;
+        } 
+
+        public class Node{      // элемент связного списка
+            int value;          // значение ноды
+            Node next;          // ссылка на следующее значение
+
+            Node previous;      // ссылка на предыдущее значение
+
+        }
+    }
+
+
+    
+
+
+
+}
