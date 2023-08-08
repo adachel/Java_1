4af4a79fdf5a103c8485b2f297400d1b1265e110
Dmitry <adachel74@gmail.com>
2023-08-08 21:37:17 +0500

    HomeWork5 final

--- GeekBrains/Education/src/main/java/HomeWork/HomeWork5/Printer.java
+++ GeekBrains/Education/src/main/java/HomeWork/HomeWork5/Printer.java
@@ -1,7 +1,6 @@
 package HomeWork.HomeWork5;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 // /**
 //     * Дан текст. Нужно отсортировать слова по длине (по возрастанию) и вывести статистику на экран.
@@ -27,34 +26,93 @@ import java.util.stream.Collectors;
 //     */
 public class Printer {
 
-    static void printStats(String text) {
+    static void printStatsVar1(String text) {   // C дефисами
         String cleanText = text.replace('.', ' ').replace(',', ' ')
-                .replace('"', ' ').toLowerCase(); // очистил от символов
-
+                .replace('"', ' ').toLowerCase(); // очистка от символов
         String[] arr = cleanText.split("\s+"); // массив строк
-        List<String> arrTemp = Arrays.stream(arr).distinct()
-                .collect(Collectors.toList()); // удаление повторяющихся элементов
 
-        Map<Integer, List<String>> stats = new TreeMap<>();
+        Map<Integer, Set<String>> stats = new TreeMap<>();
 
-        for (int i = 1; i < 100; i++){
-            List<String> temp = new ArrayList<>();
-            for (int j = 0; j < arrTemp.size(); j++) {
-                if (arrTemp.get(j).length() == i) {
-                    temp.add(arrTemp.get(j));
-                    stats.put(i, temp);
+        for (int i = 0; i < arr.length; i++){
+            Set<String> setList = new LinkedHashSet<>();
+            int len = arr[i].length();
+            for (String word: arr){
+                if (len == word.length()){
+                    setList.add(word);
+                    stats.put(len, setList);
                 }
             }
         }
+
+        for (var item : stats.entrySet()) {
+            System.out.printf("%d-> %s\n", item.getKey(), item.getValue());
+        }
+
+    }
+
+
+    static void printStatsVar2(String text) {   // без дефисов
+        String cleanText = text.replace('.', ' ').replace(',', ' ')
+                .replace('"', ' ').toLowerCase(); // очистка от символов
+        String[] arr = cleanText.split("\s+"); // массив строк
+
+        List<String> temp = new ArrayList<>();
+
+        for (String word: arr){
+            if (word.contains("-")){
+                word = word.replace('-', ' ').replaceAll("\\s+", "");
+            }
+            temp.add(word);
+        }
+
+        Map<Integer, Set<String>> stats = new TreeMap<>();
+        for (int i = 0; i < temp.size(); i++){
+            Set<String> setList = new LinkedHashSet<>();
+            int len = temp.get(i).length();
+            for (String word: temp){
+                if (len == word.length()){
+                    setList.add(word);
+                    stats.put(len, setList);
+                }
+            }
+        }
+
         for (var item : stats.entrySet()) {
             System.out.printf("%d-> %s\n", item.getKey(), item.getValue());
         }
     }
+
+
+//    static void printStats(String text) {
+//        String cleanText = text.replace('.', ' ').replace(',', ' ')
+//                .replace('"', ' ').toLowerCase(); // очистил от символов
+//        String[] arr = cleanText.split("\s+"); // массив строк
+//
+//        List<String> arrTemp = Arrays.stream(arr).distinct()
+//                .collect(Collectors.toList()); // удаление повторяющихся элементов
+//
+//        Map<Integer, List<String>> stats = new TreeMap<>();
+//
+//        for (int i = 1; i < 100; i++){
+//            List<String> temp = new ArrayList<>();
+//            for (int j = 0; j < arrTemp.size(); j++) {
+//                if (arrTemp.get(j).length() == i) {
+//                    temp.add(arrTemp.get(j));
+//                    stats.put(i, temp);
+//                }
+//            }
+//        }
+//        for (var item : stats.entrySet()) {
+//            System.out.printf("%d-> %s\n", item.getKey(), item.getValue());
+//        }
+//    }
     public static void main(String[] args) {
 
         String text = "Это мой первый текст. Он состоит из каких-то тестовых слов и нужен для того, чтобы выполнить тестовое задание GB. " +
                 "Данный текст не несет в себе какого-либо смысла, он просто содержит набор слов.";
 
-        printStats(text);
+        printStatsVar1(text);
+        System.out.println();
+        printStatsVar2(text);
     }
 }
